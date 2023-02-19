2ecf0be7dd60f0d04ced03801598976dde0ba9f3
Unai78o <108680315+Unai78o@users.noreply.github.com>
2023-02-19 18:13:21 +0100

    modified:   JS/index.html 	modified:   JS/test.html 	modified:   JS/tests.js

--- JS/tests.js
+++ JS/tests.js
@@ -1,13 +1,19 @@
-function rangeOfNumbers(startNum, endNum) {
-  if (startNum > endNum){
-    return [];
-  }else{
-    const rangeArr = rangeOfNumbers(startNum, endNum - 1)
-    rangeArr.push(endNum)
-    return rangeArr
+const s = [5, 7, 2];
+function editInPlace() {
+  // Only change code below this line
+  for (let i = 0; i < 3; i++){
+    if (i == 0){
+      s[i] = 2
+    }else if (i == 1){
+      s[i] = 5
+    }else if (i == 2){
+      s[i] = 7
+    }
   }
-  
-};
+  // Using s = [2, 5, 7] would be invalid
 
-console.log(
-  rangeOfNumbers(1, 5));
+  // Only change code above this line
+}
+editInPlace();
+
+console.log(s);
